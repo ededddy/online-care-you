b135a45b6a1e7b8893236ae287a503b8260caf4b
Eddy Lei <eddylei070300@gmail.com>
2021-11-29 00:40:21 +0800

    done

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<script lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { Person } from '@vicons/ionicons5'
+import { defineComponent } from 'vue'
+import { zhCN, dateZhCN  } from 'naive-ui'
+import Care from "./components/Care.vue"
+
+export default defineComponent({
+  components: {
+    Person,
+    Care
+  },
+  setup() {
+    return {
+      zhCN,
+      dateZhCN
+    }
+  }, 
+  data () { 
+    return {name: "", target: ""}
+  }, 
+})
+</script>
+
+<template>
+  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
+    <n-space vertical>
+      <n-h1 prefix="bar"><n-text type="primary">ONLINE 關心你</n-text></n-h1>
+      <n-input placeholder="你的名字" v-model:value="name">
+        <template #prefix>
+          <n-icon>
+            <person />
+          </n-icon>
+        </template>
+      </n-input>
+      <n-input placeholder="關心對象" v-model:value="target">
+        <template #prefix>
+          <n-icon>
+            <person />
+          </n-icon>
+        </template>
+      </n-input>
+      <Care v-if="name && target" :target="target" :name="name"/>
+    </n-space>
+  </n-config-provider>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 30px;
+}
+</style>
---
+++ src/components/Care.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps<{ name: string, target: string }>()
+</script>
+
+<template>
+  <div id="care">
+    <n-h1>
+      <n-text type="success">{{ name }}</n-text>
+      關心了
+      <n-text type="warning">{{target}}</n-text>
+    </n-h1>
+    <n-button type="primary" size="large" disabled>
+        立即截圖分享!
+    </n-button>
+  </div>
+</template>
+
+<style scoped>
+#care {
+ margin-top: 2em;
+}
+</style>
